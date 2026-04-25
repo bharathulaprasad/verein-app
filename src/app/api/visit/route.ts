d0be8ea25cef87428592baba99b5e0dcb2b45cd7
Bharathula Prasad <krishna.venkata@iiitg.ac.in>
2026-04-25 20:52:25 +0200

    resolve build error

--- src/app/api/visit/route.ts
+++ src/app/api/visit/route.ts
@@ -1,7 +1,7 @@
 import { NextResponse } from 'next/server';
-import { PrismaClient } from '@prisma/client';
+import prisma from '@/lib/prisma';
 
-const prisma = new PrismaClient();
+export const dynamic = 'force-dynamic';
 
 // This runs when a NEW visitor arrives (Increments DB)
 export async function POST() {
